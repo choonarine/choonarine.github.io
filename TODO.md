f3562d8de001beb4d5d33a304c513d7a5c9629cb
choonarine <choonarine@gmail.com>
2026-04-15 23:56:16 +0900

    minor changes

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 # TODO
 
 - [ ] Giscus 연동
+- [ ] MathJax 추가 (`remark-math`)
 - [ ] 페이지 또는 Layout 요소들 전부 Component로 묶기
 - [ ] ToC 위치 버그 수정
 - [ ] CONDUCT OF CODE 작성
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+import rss from '@astrojs/rss';
+
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+
+export const GET: APIRoute = async (context) => {
+  const posts = await getCollection('posts');
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site!,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/posts/${post.slug}/`,
+    })),
+  });
+};
