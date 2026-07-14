a212afe9a236dbccce58e5227cb8e4bf70646f10
Choo Narine <choonarine@gmail.com>
2026-07-14 13:50:54 +0900

    feat: adding OG image using takumi

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,22 +7,32 @@ import solidJs from '@astrojs/solid-js';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import * as fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import astroTakumi, { presets } from "astro-takumi";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://choonarine.github.io',
 
   integrations: [
     mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: {
-        theme: 'github-dark',
-        wrap: true
-      },
+        syntaxHighlight: 'shiki',
+        shikiConfig: {
+          theme: 'github-dark',
+          wrap: true
+        },
       remarkPlugins: [remarkMath],
       rehypePlugins: [rehypeKatex],
     }),
     sitemap(),
     solidJs(),
+    astroTakumi({
+      options: {
+        fonts: [fs.readFileSync(fileURLToPath(import.meta.resolve("pretendard/dist/web/static/woff2-subset/Pretendard-Medium.subset.woff2")))],
+      },
+      render: presets.blackAndWhite,
+    })
   ],
 
   image: {
